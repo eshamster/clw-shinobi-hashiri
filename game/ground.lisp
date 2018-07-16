b03f98f0fe982e203155e02023ab03a3af968b57
eshamster <hamgoostar@gmail.com>
2018-07-16 08:37:56 +0000

    Add event listner for scrolling

--- game/ground.lisp
+++ game/ground.lisp
@@ -4,13 +4,24 @@
         :cl-ps-ecs
         :cl-web-2d-game)
   (:export :init-ground
-           :get-ground-height)
+           :get-ground-height
+           :add-on-ground-scroll)
   (:import-from :clw-shinobi-hashiri/game/parameter
                 :get-param
                 :get-depth))
 (in-package :clw-shinobi-hashiri/game/ground)
 
 ;; TODO: Prevent overflow by scroll
+
+;; --- event --- ;;
+
+(defun.ps+ add-on-ground-scroll (entity func &optional (ground (find-ground)))
+  "Add scroll event function. The func takes entity and scroll speed as arguments.
+If the entity is deleted, the func is also deleted"
+  (setf (gethash (ecs-entity-id entity) (get-entity-param ground :on-scroll))
+        func))
+
+;; --- --- ;;
 
 (defvar.ps+ *dummy-scroll-speed* 1)
 
@@ -37,9 +48,20 @@
 
 (defun.ps+ scroll-ground (ground)
   (check-entity-tags ground :ground)
-  (with-ecs-components (point-2d) ground
-    (incf (point-2d-x point-2d)
-          (* -1 *dummy-scroll-speed*))))
+  (let ((scroll-speed *dummy-scroll-speed*)
+        (on-scroll-hash (get-entity-param ground :on-scroll)))
+    (with-ecs-components (point-2d) ground
+      (incf (point-2d-x point-2d)
+            (* -1 scroll-speed)))
+    (maphash (lambda (entity-id func)
+               (let ((entity (find-a-entity (lambda (target)
+                                              (= (ecs-entity-id target))
+                                              entity-id))))
+                 (if (find-the-entity entity)
+                     (funcall func entity scroll-speed)
+                     (register-next-frame-func
+                      (lambda () (remhash entity on-scroll-hash))))))
+             on-scroll-hash)))
 
 ;; Definition of stage
 ;; ((height distance) (height distance) ...)
@@ -72,10 +94,11 @@
      (make-point-2d)
      (make-script-2d :func (lambda (entity)
                              (scroll-ground entity)))
-     (init-entity-params))
+     (init-entity-params :on-scroll (make-hash-table)))
     (interpret-stage
      ground
      (create-stage (#ly50 #lx300)
                    (#ly-1 #lx80)
                    (#ly80 #lx300)))
-    (add-ecs-entity ground parent)))
+    (add-ecs-entity ground parent)
+    ground))
