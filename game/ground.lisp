abd4aa449c661e67621c46b93a1bd4ea4cf75b88
eshamster <hamgoostar@gmail.com>
2018-07-28 05:31:21 +0000

    Refactor: Add wall structure instead of simple height and width

--- game/ground.lisp
+++ game/ground.lisp
@@ -23,28 +23,41 @@ If the entity is deleted, the func is also deleted"
 
 ;; --- --- ;;
 
+(defvar.ps+ *next-wall-id* 0)
+
+(defstruct.ps+ (wall (:include ecs-component))
+    id height width name)
+
 (defvar.ps+ *dummy-scroll-speed* 1.5)
 
 (defun.ps+ find-ground ()
   (find-a-entity-by-tag :ground))
 
-(defun.ps+ get-ground-height (center-x width &optional (ground (find-ground)))
+(defun.ps+ get-highest-wall (center-x width &optional (ground (find-ground)))
   (check-entity-tags ground :ground)
   (let ((min-x (- center-x (* 1/2 width)))
         (max-x (+ center-x (* 1/2 width)))
         (left (point-2d-x (get-ecs-component 'point-2d ground)))
-        (result #ly-1000))
-    (dolist (pair (get-entity-param ground :stage))
-      (let ((height (car pair))
-            (right (+ left (cadr pair))))
+        (max-height #ly-1000)
+        (result nil))
+    (dolist (wall (get-entity-param ground :stage))
+      (let ((height (wall-height wall))
+            (right (+ left (wall-width wall))))
         (when (> left max-x)
-          (return-from get-ground-height result))
+          (return-from get-highest-wall result))
         (when (and (> right min-x)
                    (>= height 0)
-                   (> height result))
-          (setf result height))
+                   (> height max-height))
+          (setf max-height height)
+          (setf result wall))
         (setf left right)))
     result))
+
+(defun.ps+ get-ground-height (center-x width &optional (ground (find-ground)))
+  (let ((wall (get-highest-wall center-x width ground)))
+    (if wall
+        (wall-height wall)
+        #ly-1000)))
 
 (defun.ps+ scroll-ground (ground)
   (check-entity-tags ground :ground)
@@ -66,24 +79,28 @@ If the entity is deleted, the func is also deleted"
 ;; Definition of stage
 ;; ((height distance) (height distance) ...)
 (defmacro.ps+ create-stage (&rest def)
-  `(list ,@(mapcar (lambda (pair)
-                     `(list ,(car pair) ,(cadr pair)))
+  `(list ,@(mapcar (lambda (block-def)
+                     (destructuring-bind (height width &key name)
+                         block-def
+                       `(make-wall :id (incf *next-wall-id*)
+                                   :height ,height
+                                   :width ,width
+                                   :name ,name)))
                    def)))
 
 (defun.ps+ interpret-stage (ground stage)
   ;; TODO: Incrementaly add (and delete) models
   (let ((x 0))
-    (dolist (pair stage)
-      (let ((height (car pair))
-            (dist (cadr pair)))
+    (dolist (wall stage)
+      (with-slots (width height) wall
         (add-ecs-component
-         (make-model-2d :model (make-solid-rect :width dist
+         (make-model-2d :model (make-solid-rect :width width
                                                 :height height
                                                 :color #x000000)
                         :depth (get-depth :ground)
                         :offset (make-point-2d :x x))
          ground)
-        (incf x dist))))
+        (incf x width))))
   (set-entity-param ground :stage stage))
 
 (defun.ps+ init-ground (parent)
