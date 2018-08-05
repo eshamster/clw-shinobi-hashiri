e80ff517f6d6c87cb8ace37e379d1c20da43e2d2
eshamster <hamgoostar@gmail.com>
2018-08-05 15:29:39 +0000

    Update character parameter

--- game/parameter.lisp
+++ game/parameter.lisp
@@ -31,11 +31,11 @@
    (:field (:width field-width :height field-height)
     :shinobi (:width #ly40 :height #ly40
               :jump (:speed 5 :max-time 18)
-              :on-ground (:default-x #lx200 :return-speed #lx3)
+              :on-ground (:default-x #lx300 :return-speed #lx5)
               ;; TODO: Speed to y direction should be specified by #ly instead of #lx
               :glide (:gravity-rate 0.15 :first-y-speed #lx2.1 :max-fall-speed #lx3.5
                       :back-speed #lx0.4)
-              :climb-jump (:min-speed #lx5 :max-speed #lx7 :duration 15))
+              :climb-jump (:min-speed #lx6 :max-speed #lx10 :duration 20))
     :gravity (:accell #ly0.45 :max-speed 20)
     :ground (:scroll (:first #lx2 :max #lx5 :accell #lx0.001)))))
 
